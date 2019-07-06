e18788ca7ed37ba616ec0cad7524221d8e99fea8
m4riusz <sariuszm@gmail.com>
2019-07-06 18:47:49 +0200

    Initial Commit

---
+++ ChuckNorrisJokesAppRedux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJokesAppRedux
+//
+//  Created by Mariusz Sut on 06/07/2019.
+//  Copyright © 2019 Mariusz Sut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
